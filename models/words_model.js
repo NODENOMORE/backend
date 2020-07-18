aa837c9906f43ed2aa89e14c996c706d7df4b9ac
brokenhoax <brokenhoax@outlook.com>
2020-07-18 04:09:38 -0500

    CRUD was hard...

--- models/words_model.js
+++ models/words_model.js
@@ -1,25 +1,32 @@
 const mysql = require('../config/connection');
 
-exports.selectAll = async () => {
-  const connection = await mysql.connect();
-  const [data] = await connection.query(`SELECT * FROM mywords`);
-  return data;
-}
-
-// exports.selectOne = async (word) => {
-//   const connection = await mysql.connect();
-//   const [data] = await connection.query(`SELECT * FROM mywords WHERE word = ?`, [word]);
-//   return data;
-// }
-
 exports.insert = async (word_obj) => {
   const connection = await mysql.connect();
   const [data] = await connection.query(`INSERT INTO mywords SET ?`, word_obj);
   return data;
 }
 
-// exports.update = async (id, word_obj) => {
-//   const connection = await mysql.connect();
-//   const [data] = await connection.query(`UPDATE mywords SET ? WHERE id = ?`, [word-obj, id]);
-//   return data;
-// }
+exports.selectAll = async () => {
+  const connection = await mysql.connect();
+  const [data] = await connection.query(`SELECT * FROM mywords`);
+  return data;
+}
+
+exports.selectById = async (id) => {
+  const connection = await mysql.connect();
+  const [data] = await connection.query(`SELECT * FROM mywords WHERE id = ?`, [id]);
+  return data[0];
+}
+
+exports.update = async (id, word_obj) => {
+  const connection = await mysql.connect();
+  const [data] = await connection.query(`UPDATE mywords SET ? WHERE id = ?`,  [word_obj, id]);
+  return data;
+}
+
+exports.delete = async (id) => {
+  const connection = await mysql.connect();
+  const [data] = await connection.query(`DELETE FROM mywords WHERE id = ?`, [id]);
+  // console.log('Was your stuff deleted?')
+  return data;
+}
