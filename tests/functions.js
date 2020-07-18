17cdc33e8eee381093941b8fe3cda1c3a2be17b5
brokenhoax <brokenhoax@outlook.com>
2020-07-17 22:12:01 -0500

    ...

--- tests/functions.js
+++ tests/functions.js
@@ -1,17 +1,17 @@
-// class Word {
-//   constructor(word_obj) {
-//     this.id = 1;
-//     this.word = 'helicopter'; 
-//     this.definition = 'a type of aircraft which derives both lift and propulsion from one or more sets of horizontally revolving overhead rotors. It is capable of moving vertically and horizontally, the direction of motion being controlled by the pitch of the rotor blades.'; 
-//     this.favorite = 0;
-//     this.category = 'dirigibles';
-//     this.notes = 'helicopters are dangerous!';
-//   }   
+class Word {
+  constructor(word_obj) {
+    this.id = 1;
+    this.word = 'helicopter'; 
+    this.definition = 'a type of aircraft which derives both lift and propulsion from one or more sets of horizontally revolving overhead rotors. It is capable of moving vertically and horizontally, the direction of motion being controlled by the pitch of the rotor blades.'; 
+    this.favorite = 0;
+    this.category = 'dirigibles';
+    this.notes = 'helicopters are dangerous!';
+  }   
 
-//   getLength(){
-//     this.word.length;
-//   }
+  getLength(){
+    this.word.length;
+  }
 
-// }
+}
 
-// module.exports = functions;
+module.exports = functions;
