62c92a8f5ded63b1696e74ec047e1caf3155946c
brokenhoax <brokenhoax@outlook.com>
2020-07-17 22:00:19 -0500

    Unit Test

--- functions.js
+++ functions.js
@@ -1,25 +1,25 @@
-class Word {
-  constructor(word_obj) {
-    this.id = 1;
-    this.word = 'helicopter'; 
-    this.definition = 'a type of aircraft which derives both lift and propulsion from one or more sets of horizontally revolving overhead rotors. It is capable of moving vertically and horizontally, the direction of motion being controlled by the pitch of the rotor blades.'; 
-    this.favorite = 0;
-    this.category = 'dirigibles';
-    this.notes = 'helicopters are dangerous!';
-  }   
+// class Word {
+//   constructor(word_obj) {
+//     this.id = 1;
+//     this.word = 'helicopter'; 
+//     this.definition = 'a type of aircraft which derives both lift and propulsion from one or more sets of horizontally revolving overhead rotors. It is capable of moving vertically and horizontally, the direction of motion being controlled by the pitch of the rotor blades.'; 
+//     this.favorite = 0;
+//     this.category = 'dirigibles';
+//     this.notes = 'helicopters are dangerous!';
+//   }   
 
-  getLength(){
-    this.word.length;
-  }
+//   getLength(){
+//     this.word.length;
+//   }
 
-}
+// }
 
-test('Checks if the length of the word is less than 10 letters long.', () => {
-  const werd = new Word();
+// test('Checks if the length of the word is less than 10 letters long.', () => {
+//   const werd = new Word();
 
 
-  // Add function to test that word in dictionary has the correct definition
+//   // Add function to test that word in dictionary has the correct definition
 
-});
+// });
 
-module.exports = functions;
+// module.exports = functions;
